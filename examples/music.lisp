27d616bf81425de8bc6e1bb06785fc79fa6ba0ea
Aardbei <megaloler9000@gmail.com>
2018-03-27 19:51:11 -0500

    music lib environment parent

--- examples/music.lisp
+++ examples/music.lisp
@@ -7,15 +7,15 @@
 (defsong (song-demo
 	  :title "Music Demo"
 	  :description "An example song for Game Boy.")
-  (with-key 'e♭-major
-    (seq (apply #'mapcar (lambda (a b c)
-			  (voices (list a b c)))
-		(make-chorale
-		 (harmony '(I IV64 V43 I6 I6 IV IV I64 I64 V V65/vi V/vi
-			    vi visus2 iiimin iv iidom7 I iimin7 I6 IV I64 Vsus4 V7))
-		 3))
-	 '(1/2 1 1 1/3 1 1 1 2/3 2 1/2 2/3 2
-	   1/2 1 1 1/3 1 1 1 1 1 1/2 1 1 1/4 1/4))))
+  (with-key ('f-major)
+      (seq (apply #'mapcar (lambda (a b c)
+			     (voices (list a b c)))
+		  (make-chorale
+		   (harmony '(I IV64 V43 I6 I6 IV IV I64 I64 V V65/vi V/vi
+			      vi visus2 iiimin iv iidom7 I iimin7 I6 IV I64 Vsus4 V7))
+		   3))
+	   '(1/2 1 1 1/3 1 1 1 2/3 2 1/2 2/3 2
+	     1/2 1 1 1/3 1 1 1 1 1 1/2 1 1 1/4 1/4))))
 
 (with-gb-out (("../examples/music.gb" :vblank (addr :vblank))
 	      :title "Music Demo")
@@ -64,7 +64,7 @@
   (ldm +nr11+ #b01000000)
   (ldm +nr21+ #b01000000)
   (with-label :loop
-    (gb/play song-demo)
+    (gb/play (event song-demo))
     (jp (addr :loop)))
   
   ;; the message to display on screen
